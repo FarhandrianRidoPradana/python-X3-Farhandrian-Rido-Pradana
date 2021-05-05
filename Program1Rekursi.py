957720130f2245168cf9e75d4fd20f8b9e94d263
Farhandrian <farhandrianrido@gmail.com>
2021-05-05 13:37:53 +0700

    Program Rekursi

---
+++ Program1Rekursi.py
@@ -0,0 +1,9 @@
+def konversi(desimal):
+    if desimal > 1:
+        konversi(desimal//2)
+    print(desimal % 2, end =' ')
+ 
+desimal = int(input('input nilai desimal : '))
+ 
+konversi(desimal)
+print()
